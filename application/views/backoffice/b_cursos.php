7e6f0f3a7c04d4a5d481bd528947279ef11c3c4c
software.contreras@gmail.com <software.contreras@gmail.com>
2020-05-25 03:11:04 -0500

    cambios en backoffice

--- application/views/backoffice/b_cursos.php
+++ application/views/backoffice/b_cursos.php
@@ -37,7 +37,7 @@
                             </div>
                             <div class="stm_lms_courses__archive_wrapper">
                                 <div class="stm_lms_courses__archive_filter"> 
-                                    <form action="<?php echo site_url().'backoffice/cursos/categorias';?>" method="get">
+                                    <form action="<?php echo site_url() . 'backoffice/cursos/categorias'; ?>" method="get">
                                         <div class="stm_lms_courses__archive_filters">
                                             <div class="stm_lms_courses__filter stm_lms_courses__category">
                                                 <div class="stm_lms_courses__filter_heading">
@@ -49,7 +49,7 @@
                                                         <div class="stm_lms_courses__filter_category"> 
                                                             <label class="stm_lms_styled_checkbox"> 
                                                                 <span class="stm_lms_styled_checkbox__inner"> 
-                                                                    <input type="checkbox" value="<?php echo $value->category_id;?>" name="category[]"/> 
+                                                                    <input type="checkbox" value="<?php echo $value->category_id; ?>" name="category[]"/> 
                                                                     <span><i class="fa fa-check"></i> </span> 
                                                                 </span> 
                                                                 <span><?php echo $value->name; ?> </span> 
@@ -77,63 +77,77 @@
                                     </form>
                                 </div>
                                 <div class="stm_lms_courses stm_lms_courses__archive">
-                                    <div class="stm_lms_courses__grid stm_lms_courses__grid_3 stm_lms_courses__grid_center archive_grid stm_lms_courses__grid_found_31" data-pages="3">
-                                        <?php foreach ($obj_courses as $value) { ?>
-                                            <div class="stm_lms_courses__single stm_lms_courses__single_animation has-sale style_1 ">
-                                                <div class="stm_lms_courses__single__inner">
-                                                    <div class="stm_lms_courses__single--image">
-                                                        <div class="stm_lms_post_status heading_font new"> Nuevo</div>
-                                                        <a href="<?php echo site_url() . "cursos/$value->category_slug/$value->slug"; ?>" class="heading_font" data-preview="Vista Previa del Curso">
-                                                            <div>
-                                                                <div class="stm_lms_lazy_image stm_lms_lazyloaded stm_lms_lazy_image__lazyloaded">
-                                                                    <img data-srcset="<?php echo site_url() . "static/cms/img/cursos/$value->img"; ?> 2x" class="lazyload " data-src="<?php echo site_url() . "static/cms/img/cursos/$value->img"; ?>" width="272" height="161" alt="<?php echo $value->name; ?>" title="<?php echo $value->name; ?>"/>
+                                    <?php if (!empty($obj_courses)) { ?>
+                                        <div class="stm_lms_courses__grid stm_lms_courses__grid_3 stm_lms_courses__grid_center archive_grid stm_lms_courses__grid_found_31" data-pages="3">
+    <?php foreach ($obj_courses as $value) { ?>
+                                                <div class="stm_lms_courses__single stm_lms_courses__single_animation has-sale style_1 ">
+                                                    <div class="stm_lms_courses__single__inner">
+                                                        <div class="stm_lms_courses__single--image">
+                                                            <div class="stm_lms_post_status heading_font new"> Nuevo</div>
+                                                            <a href="<?php echo site_url() . "cursos/$value->category_slug/$value->slug"; ?>" class="heading_font" data-preview="Vista Previa del Curso">
+                                                                <div>
+                                                                    <div class="stm_lms_lazy_image stm_lms_lazyloaded stm_lms_lazy_image__lazyloaded">
+                                                                        <img data-srcset="<?php echo site_url() . "static/cms/img/cursos/$value->img"; ?> 2x" class="lazyload " data-src="<?php echo site_url() . "static/cms/img/cursos/$value->img"; ?>" width="272" height="161" alt="<?php echo $value->name; ?>" title="<?php echo $value->name; ?>"/>
+                                                                    </div>
+                                                                </div>
+                                                            </a>
+                                                        </div>
+                                                        <div class="stm_lms_courses__single--inner">
+                                                            <div class="stm_lms_courses__single--terms">
+                                                                <div class="stm_lms_courses__single--term"> <?php echo $value->category_name; ?></div>
+                                                            </div>
+                                                            <div class="stm_lms_courses__single--title">
+                                                                <a href="<?php echo site_url() . "cursos/$value->category_slug/$value->slug"; ?>">
+                                                                    <h5><?php echo str_to_mayusculas($value->name); ?></h5>
+                                                                </a>
+                                                            </div>
+                                                            <div class="stm_lms_courses__single--meta">
+                                                                <div class="stm_lms_courses__hours"> <i class="stmlms-lms-clocks"></i> <span><?php echo $value->time; ?> horas</span></div>
+                                                                <div class="stm_lms_courses__single--price heading_font"> 
+                                                                    <span><?php echo $value->price_del; ?></span><strong><?php echo $value->price; ?></strong>
                                                                 </div>
                                                             </div>
-                                                        </a>
-                                                    </div>
-                                                    <div class="stm_lms_courses__single--inner">
-                                                        <div class="stm_lms_courses__single--terms">
-                                                            <div class="stm_lms_courses__single--term"> <?php echo $value->category_name; ?></div>
                                                         </div>
-                                                        <div class="stm_lms_courses__single--title">
-                                                            <a href="<?php echo site_url() . "cursos/$value->category_slug/$value->slug"; ?>">
-                                                                <h5><?php echo str_to_mayusculas($value->name); ?></h5>
-                                                            </a>
-                                                        </div>
-                                                        <div class="stm_lms_courses__single--meta">
-                                                            <div class="stm_lms_courses__hours"> <i class="stmlms-lms-clocks"></i> <span><?php echo $value->time;?> horas</span></div>
-                                                            <div class="stm_lms_courses__single--price heading_font"> 
-                                                                <span><?php echo $value->price_del; ?></span><strong><?php echo $value->price; ?></strong>
+                                                        <div class="stm_lms_courses__single--info">
+                                                            <div class="stm_lms_courses__single--info_author">
+                                                                <div class="stm_lms_courses__single--info_author__avatar"> 
+                                                                    <img alt="profesor" src="<?php echo site_url() . 'static/page_front/images/profesor.png'; ?>" class="avatar avatar-215 photo" width="215" height="215">
+                                                                </div>
+                                                                <div class="stm_lms_courses__single--info_author__login">Instructor: U-linex</div>
                                                             </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="stm_lms_courses__single--info">
-                                                        <div class="stm_lms_courses__single--info_author">
-                                                            <div class="stm_lms_courses__single--info_author__avatar"> 
-                                                                <img alt="profesor" src="<?php echo site_url() . 'static/page_front/images/profesor.png'; ?>" class="avatar avatar-215 photo" width="215" height="215">
+                                                            <div class="stm_lms_courses__single--info_title">
+                                                                <a href="<?php echo site_url() . "cursos/$value->category_slug/$value->slug"; ?>">
+                                                                    <h4><?php echo strtoupper($value->name); ?></h4>
+                                                                </a>
                                                             </div>
-                                                            <div class="stm_lms_courses__single--info_author__login">Instructor: U-linex</div>
-                                                        </div>
-                                                        <div class="stm_lms_courses__single--info_title">
-                                                            <a href="<?php echo site_url() . "cursos/$value->category_slug/$value->slug"; ?>">
-                                                                <h4><?php echo strtoupper($value->name); ?></h4>
-                                                            </a>
-                                                        </div>
-                                                        <div class="stm_lms_courses__single--info_excerpt"> 
-                                                            <?php echo corta_texto($value->description, 300); ?>
-                                                        </div>
-                                                        <div class="stm_lms_courses__single--info_meta">
-                                                            <!--<div class="stm_lms_course__meta"> <i class="stmlms-cats"></i> 10 Materiales</div>-->
-                                                            <div class="stm_lms_course__meta"> <i class="stmlms-lms-clocks"></i> <span><?php echo $value->time;?> horas</div>
-                                                        </div>
-                                                        <div class="stm_lms_courses__single--info_preview"> 
-                                                            <a href="<?php echo site_url() . "cursos/$value->category_slug/$value->slug"; ?>" title="<?php echo $value->name; ?>" class="heading_font"> Vista previa de este curso</a>
+                                                            <div class="stm_lms_courses__single--info_excerpt"> 
+        <?php echo corta_texto($value->description, 300); ?>
+                                                            </div>
+                                                            <div class="stm_lms_courses__single--info_meta">
+                                                                <!--<div class="stm_lms_course__meta"> <i class="stmlms-cats"></i> 10 Materiales</div>-->
+                                                                <div class="stm_lms_course__meta"> <i class="stmlms-lms-clocks"></i> <span><?php echo $value->time; ?> horas</div>
+                                                            </div>
+                                                            <div class="stm_lms_courses__single--info_preview"> 
+                                                                <a href="<?php echo site_url() . "cursos/$value->category_slug/$value->slug"; ?>" title="<?php echo $value->name; ?>" class="heading_font"> Vista previa de este curso</a>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        <?php } ?>
-                                    </div>
+    <?php } ?>
+                                        </div>
+<?php } else { ?>
+                                        <div class="stm_lms_courses__grid stm_lms_courses__grid_3 stm_lms_courses__grid_center archive_grid stm_lms_courses__grid_found_31" data-pages="3">    
+                                            <h4 class="stm_lms_course__notfound">
+                                                <i class="lnricons-ghost"></i>
+                                                <div>No hay cursos encontrado.</div>
+                                                <a href="<?php echo site_url() . 'backoffice/cursos'; ?>" class="stm_lms_courses__filter_reset">
+                                                    <i class="lnr lnr-undo"></i>
+                                                    <span>Borrar todo</span>
+                                                </a>
+                                            </h4>
+
+                                        </div>
+<?php } ?>
                                 </div>
                             </div>
                         </div>
@@ -144,7 +158,7 @@
             <!--#main-->
         </div>
         <!--#wrapper-->
-        <?php $this->load->view("footer"); ?>
+<?php $this->load->view("footer"); ?>
         <link rel='stylesheet' id='stm-lms-courses-css' href='<?php echo site_url() . 'static/backoffice/css/footer/courses.css?ver=81'; ?>' type='text/css' media='all' />
         <link rel='stylesheet' id='stm-lms-courses_filter-css' href='<?php echo site_url() . 'static/backoffice/css/footer/courses_filter.css?ver=81'; ?>' type='text/css' media='all' />
         <link rel='stylesheet' id='stm-lms-user-css' href='<?php echo site_url() . 'static/backoffice/css/footer/user.css?ver=75'; ?>' type='text/css' media='all' />
@@ -152,7 +166,6 @@
         <link rel='stylesheet' id='stm-header_mobile-account-css' href='<?php echo site_url() . 'static/backoffice/css/footer/account.css?ver=3.2'; ?>' type='text/css' media='all' />
         <link rel='stylesheet' id='stm-lms_categories_megamenu-style_1-css' href='<?php echo site_url() . 'static/backoffice/css/footer/lms_categories_megamenu.css?ver=3.2'; ?>' type='text/css' media='all' />
         <script src='<?php echo site_url() . 'static/backoffice/js/bootstrap.min.js?ver=3.2'; ?>'></script>
-
         <script src='<?php echo site_url() . 'static/backoffice/js/courses_filter.js?ver=81'; ?>'></script>
         <script src='<?php echo site_url() . 'static/backoffice/js/custom.js?ver=3.2'; ?>'></script>
         <script src='<?php echo site_url() . 'static/backoffice/js/select2.full.min.js?ver=3.2'; ?>'></script>
