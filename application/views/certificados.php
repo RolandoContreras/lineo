2f6b9bc63b2abbab65ac449ae9dbcd5a390c5705
unknown <software.contreras@gmail.com>
2021-03-07 09:17:39 -0500

    agregar lazy en certificado view

--- application/views/certificados.php
+++ application/views/certificados.php
@@ -68,6 +68,7 @@
             </div>
         </div>
         <?php $this->load->view("footer_2"); ?>
+        <script defer src="<?php echo site_url() . 'static/page_front/seo/lazysizes.min.js'; ?>"></script>
         <script defer src="<?php echo site_url() . 'static/page_front/js/script/certificate.js'; ?>"></script>
         <script src="<?php echo site_url() . 'static/page_front/js/autoptimize_54ab.js'; ?>"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
